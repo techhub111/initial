74e05ccdb05fb61c5c0b6a4d991c03a1deae5651
techhub111 <createapi1@gmail.com>
2017-04-19 21:04:00 +0530

    removed

--- retailmanagerapi/src/main/java/com/assignment/microservice/dao/RetailManagerDao.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.microservice.dao;
-
-import com.assignment.microservice.entity.Shop;
-import com.assignment.microservice.model.LatLng;
-
-public interface RetailManagerDao {
-	public Shop getShopWithinMinimumDistance(LatLng latLng, Iterable<Shop> shopsList);
-	public Shop getShop(String shopName);
-}
